254472825ec86231515183d858bd2ae2da3307a8
SudeepTrainer <info@icenbake.com>
2024-08-08 16:46:14 +0530

    updated 08-aug

--- TODOApp/app.js
+++ TODOApp/app.js
@@ -5,6 +5,8 @@ const TodoModel = require("./model/ToDo");
 const PORT = 5000;
 
 const app = express();
+// middleware 
+app.use(express.json());
 const start = async () => {
   try {
     await connectDb(process.env.DB_URL);
@@ -23,21 +25,30 @@ app.get("/", (req, res) => {
 
 // CREATE/POST TODO
 app.post("/api/v1/todos", async (req, res) => {
-  const { title, description, completed } = req.body;
-  const ToDo = await TodoModel.create({
-    title: title,
-    description: description,
-    completed: completed,
-  });
-  console.log(ToDo);
-  // add to db
-  res.status(201).json({ message: "TODO created" });
+  try {
+    const ToDo = await TodoModel.create(req.body);
+    if (!ToDo) {
+      res.status(404).json({ message: "Couldn't create" });
+    }
+    // add to db
+    res.status(201).json({ message: ToDo });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 // READ/ GET SINGLE TODO
-app.get("/api/v1/todos/:id", (req, res) => {
-  const { id } = req.params;
-  // fetch data from db
-  res.status(200).json({ message: "TODO fetched" });
+app.get("/api/v1/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // fetch data from db
+    const Todo = await TodoModel.findById(id);
+    if (!Todo) {
+      res.status(404).json("Not found");
+    }
+    res.status(200).json({ message: Todo });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 // UPDATE/ PATCH TODO
 app.patch("/api/v1/todos/:id", (req, res) => {
@@ -47,13 +58,27 @@ app.patch("/api/v1/todos/:id", (req, res) => {
   res.status(200).json({ message: "TODO updated" });
 });
 // DELETE/ delete TODO
-app.delete("/api/v1/todos/:id", (req, res) => {
-  const { id } = req.params;
-  // delete the todo in db for given id
-  res.status(200).json({ message: "TODO deleted" });
+app.delete("/api/v1/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // delete the todo in db for given id
+    const deletedTodo = await TodoModel.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json({ message: "TODO deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 // GET ALL TODOS
-app.get("/api/v1/todos", (req, res) => {
-  // get all the todos from DB
-  res.status(200).json({ message: "ALL TODOS Fetched" });
+app.get("/api/v1/todos", async (req, res) => {
+  try {
+    // get all the todos from DB
+    const todos = await TodoModel.find({});
+    console.log(todos);
+    res.status(200).json({ message: todos });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
--- TODOApp/model/ToDo.js
+++ TODOApp/model/ToDo.js
@@ -2,10 +2,25 @@ const mongoose = require("mongoose");
 // creating the structure of the document
 // defining the fields and their types
 const ToDoSchema = new mongoose.Schema({
-  createdOn: Date,
-  title: String,
-  description: String,
-  completed: Boolean,
+  createdOn: {
+    type: Date,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 20,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 120,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 // model is an instance of the schema
 const TodoModel = mongoose.model("ToDo", ToDoSchema);
